a431ddc5cca764826f802ac1e4e4aed440b28a9b
Aaron Lomibao <aaron.lomibao09@gmail.com>
2023-12-30 15:13:14 +0800

    environment variable added

--- util/staticData.js
+++ util/staticData.js
@@ -1,1 +1,10 @@
-export const offense = ["Assualt", "Robbery", "Sexual Harrasment", "Others"]
+export const offense = [
+  "Assualt",
+  "Robbery",
+  "Burglary and Break-Ins",
+  "Sexual Harrasment",
+  "Missing Person/s",
+  "Vandalism and Property Damage",
+  "Suspicious Persons or Vehicles",
+  "Others",
+];
